4d77e92087ffce18bb76a4fe9cc8f200df983fa9
teabagg2 <tehilla086@gmail.com>
2026-04-28 17:16:25 -0400

    routine

---
+++ my_daily_routine_printer.py
@@ -0,0 +1,34 @@
+# Title
+print("=== MY DAILY ROUTINE ===\n")
+
+# Organised daily routine
+print("Morning:")
+print("1. Wake up at 6:00 AM")
+print("2. Brush teeth and get ready")
+print("3. Eat breakfast")
+
+print("\nSchool Time:")
+print("4. Go to school")
+print("5. Attend classes and learn")
+
+print("\nAfternoon:")
+print("6. Come home and relax")
+print("7. Do homework and study")
+
+print("\nEvening:")
+print("8. Play games or watch TV")
+print("9. Spend time with family")
+print("10. Go to bed\n")
+
+# Calculation part
+print("=== WEEKLY STUDY TIME ===")
+
+study_hours_per_day = 2
+school_days = 5
+
+# Multiplication to find total
+total_study_hours = study_hours_per_day * school_days
+
+print("Study hours per day:", study_hours_per_day)
+print("Number of school days:", school_days)
+print("Total study hours in a week:", total_study_hours, "hours")
