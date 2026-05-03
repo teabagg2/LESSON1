2f65a84fd8379c3817c5b02630f60c2a344db711
teabagg2 <tehilla086@gmail.com>
2026-05-02 21:51:48 -0400

    class2

---
+++ class2.py
@@ -0,0 +1,26 @@
+import keyword
+
+print("the python keywords are")
+print(keyword.kwlist)
+
+
+print ("hello world")
+
+print ("my name is Tehilla")
+
+print ("4")
+
+print ("i like coding")
+
+y = 5
+x = 4
+print ("x+y",x+y)
+
+
+print ("hello \n world")
+print ("my name is \n Tehilla")
+print ("my favorite colour is \n blue")
+
+name = input ("what is your name?")
+print ("hello", name)
+
