1b51903fe5a5a1e329ba217fbb315bcf8dfbecf0
teabagg2 <tehilla086@gmail.com>
2026-05-08 12:07:37 -0400

    birthday

---
+++ birthday.py
@@ -0,0 +1,24 @@
+# Storing birthdays of Raj's friends
+
+friend1 = "Aman"
+birthday1 = "12 January"
+
+friend2 = "Riya"
+birthday2 = "5 March"
+
+friend3 = "Karan"
+birthday3 = "18 July"
+
+friend4 = "Sneha"
+birthday4 = "25 September"
+
+friend5 = "Rahul"
+birthday5 = "30 December"
+
+# Printing the birthdays
+
+print(friend1, "-", birthday1)
+print(friend2, "-", birthday2)
+print(friend3, "-", birthday3)
+print(friend4, "-", birthday4)
+print(friend5, "-", birthday5)
